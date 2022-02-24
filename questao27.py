784f8804dadf30ce8d72e37ce084f7d4cb6b5815
Felipe Albuquerque de Carvalho <60708056+felipealbuq@users.noreply.github.com>
2022-02-24 19:39:15 -0300

    Update questao27.py

--- questao27.py
+++ questao27.py
@@ -1,7 +1,7 @@
 # Faça um programa que recebe uma letra, escreva na tela se essa letra é ou não uma vogal.
 
 letra = str(input('Digite uma letra qualquer do alfabeto:\n')).upper()
-vogais = ['A','E','O','U']
+vogais = ['A','E','I','O','U]
 
 if letra in vogais:
     print(f'{letra} é vogal.\n')
