b772532c7bec66cb9cc8ceb43743c8e790d7d642
Felipe Albuquerque de Carvalho <felipealbuquerque@MacBook-Air-de-Felipe.local>
2022-02-26 17:29:13 -0300

    Questão 42 adicionada

---
+++ questao42.py
@@ -0,0 +1,9 @@
+# Escreva um programa que pergunta um número ao usuário e mostra sua tabuada completa
+# (de 1 até 10).
+
+n = float(input('Digite um número qualquer:\n'))
+cont = 1
+for c in range(1,11):
+    print(f'{n} * {cont} = {n * cont}')
+
+    cont += 1
